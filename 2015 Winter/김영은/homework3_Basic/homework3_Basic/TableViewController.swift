4bf1d223ec79b0633ba94274eedbc1b3d2478f4d
youngeun <citygxoxo@gmail.com>
2016-01-18 20:41:40 +0900

    added requestAndResponseHandling 접속성공

--- 2015 Winter/김영은/homework3_Basic/homework3_Basic/TableViewController.swift
+++ 2015 Winter/김영은/homework3_Basic/homework3_Basic/TableViewController.swift
@@ -8,23 +8,48 @@
 
 import UIKit
 import SwiftyJSON
+import Alamofire
 
 class TableViewController: UITableViewController {
 
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "하이류"  // 네비게이션 에서 타이틀
         
+        
+        requestAndResponseHandling()
         setUpTableView()
-        
-        
-        
     }
     
    
-    
+    func requestAndResponseHandling() {
+        let postEndpoint: String = "http://jsonplaceholder.typicode.com/photos/1"
+        Alamofire.request(.GET, postEndpoint)
+            .responseJSON { response in
+                guard response.result.error == nil else {
+                    // got an error in getting the data, need to handle it
+                    print("error calling GET on /photos/1")
+                    print(response.result.error!)
+                    return
+                }
+                
+                if let value: AnyObject = response.result.value {
+                    // handle the results as JSON, without a bunch of nested if loops
+                    let post = JSON(value)
+                    // now we have the results, let's just print them though a tableview would definitely be better UI:
+                    print("The post is: " + post.description)
+                    if let title = post["title"].string {
+                        // to access a field:
+                        print("The title is: " + title)
+                    } else {
+                        print("error parsing /photos/1")
+                    }
+                }
+        }
+    }
     
     func setUpTableView(){
         tableView.dataSource = self
